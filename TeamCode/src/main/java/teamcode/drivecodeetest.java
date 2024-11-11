7800fdc2b8bd5f4d123f1bb4ac565fe298d18a84
konohex <jasonli283e@gmail.com>
2024-11-10 20:28:08 -0600

    onlydrive

---
+++ TeamCode/src/main/java/teamcode/drivecodeetest.java
@@ -0,0 +1,49 @@
+package teamcode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.internal.system.Deadline;
+
+import java.util.concurrent.TimeUnit;
+
+import teamcode.drive.SampleMecanumDrive;
+
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+@TeleOp(name = "drivecode", group = "TeleOp")
+
+public class drivecodeetest extends LinearOpMode {
+    private DriveControl_NanoTorjan driveControl;
+    //private DriveControl driveControl;
+
+    private controls_NanoTrojans g2control;
+
+    private resources_NanoTrojans resources;
+    @Override
+    public void runOpMode()  throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        waitForStart();
+        drive.setWeightedDrivePower(
+                new Pose2d(
+                        -gamepad1.left_stick_y,
+                        -gamepad1.left_stick_x,
+                        -gamepad1.right_stick_x
+                )
+        );
+        drive.update();
+        Pose2d poseEstimate = drive.getPoseEstimate();
+    }}
+
+
+
+
+
+
